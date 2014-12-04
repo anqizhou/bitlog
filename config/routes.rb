a6ef5ef62f5caa3988cfd0ddac6b5c794ddde0aa
Anqi Zhou <anqizhou89@gmail.com>
2014-12-04 16:15:14 +0800

    Deleted edit and update actions for comments

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   end
 
   resources :bits, only: [:show, :edit, :update, :destroy]
-  resources :comments, only: [:show, :edit, :update, :destroy]
+  resources :comments, only: [:destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
