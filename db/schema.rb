648cd25f276b53fd906d5f0420969234a0d4bf04
Anqi Zhou <anqizhou89@gmail.com>
2014-12-16 22:43:05 +0800

    editing create user page with profile pic upload

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141216092929) do
+ActiveRecord::Schema.define(version: 20141216104641) do
 
   create_table "bits", force: true do |t|
     t.string   "content"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 20141216092929) do
   create_table "comments", force: true do |t|
     t.string   "sentence"
     t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "friendships", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "friend_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
